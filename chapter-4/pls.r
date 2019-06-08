95dd25d18788c1a2535d6263c6f33ba20ec5b71f
USER NAME <mark.t.douglas@gmail.com>
2019-06-08 10:13:50 +0100

    diffing to book

--- chapter-4/pls.r
+++ chapter-4/pls.r
@@ -7,15 +7,41 @@ ChemicalManufacturingProcess
 set.seed(666)
 
 Predictors <- ChemicalManufacturingProcess %>% select(-Yield)
-Response <- ChemicalManufacturingProcess$Yield
+Response <- ChemicalManufacturingProcess %>% pull(Yield)
 
 x <- caret::train(Predictors,
                   Response,
                   method = "pls",
                   tuneLength = 10,
-                  preProcess = c("knnImpute","pca"),
+                  preProcess = c("pca","knnImpute"),
                   trControl = caret::trainControl(method = "repeatedcv",
                                                   repeats = 5))
-x$results
 
-
+data <- x$results %>%
+  select(ncomp,Rsquared,RsquaredSD) %>%
+  mutate(RsquaredSE = RsquaredSD / sqrt(5 * 10)) %>%
+  select(-RsquaredSD) %>%
+  mutate(source = "me") %>%
+  as.tibble %>%
+  bind_rows(tribble(
+    ~ncomp     ,~Rsquared , ~RsquaredSE  
+    ,          1 ,    0.444 ,      0.0272 
+    ,          2 ,    0.500 ,      0.0298 
+    ,          3 ,    0.533 ,      0.0302 
+    ,          4 ,    0.545 ,      0.0308 
+    ,          5 ,    0.542 ,      0.0322 
+    ,          6 ,    0.537 ,      0.0327 
+    ,          7 ,    0.534 ,      0.0333 
+    ,          8 ,    0.534 ,      0.0330
+    ,          9 ,    0.520 ,      0.0326 
+    ,         10 ,    0.507 ,      0.0324 
+  ) %>%
+    mutate(source = "book")) %>%
+  gather(key=key,value=value,-source,-ncomp)
+  
+data %>%  
+  ggplot(aes(x=ncomp,y=value,colour=source))+
+  geom_point()+
+  geom_line()+
+  facet_grid(key ~ .,scales="free")+
+  scale_x_continuous(limits = c(1,10), breaks = 1:10)
