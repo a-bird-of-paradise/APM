24f4ea122da71b3ffaa197f12a0f177a7419a50f
USER NAME <mark.t.douglas@gmail.com>
2019-09-09 07:26:48 +0100

    initial model fitting and creating various plots

--- chapter-07/7.2/various-fits.r
+++ chapter-07/7.2/various-fits.r
@@ -24,11 +24,21 @@ knnModel <- caret::train(x = predictors,
 
 knnPred <- predict(knnModel, newdata = testing_predictors)
 
-caret::postResample(pred = knnPred, obs = testing_observations)
+postResampTbl <- caret::postResample(pred = knnPred, obs = testing_observations) %>%
+  enframe %>%
+  spread(key=name,value=value) %>%
+  mutate(what = "knn")
 
-tibble(obs = testing_observations,
+knn_plot <- tibble(obs = testing_observations,
        pred = knnPred) %>%
   ggplot(aes(x=obs,y=pred))+geom_point()+geom_abline()
+
+ggsave(knn_plot,
+       filename = file.path(output_directory,
+                            "knn-pred-plot.png"),
+       width = 8,
+       height = 6,
+       dpi = 100)
 
 # MARS
 
@@ -39,15 +49,119 @@ MARSModel <- caret::train(x = predictors %>% as.data.frame,
 
 MARSPred <- predict(MARSModel, newdata = testing_predictors) %>% as.vector
 
-caret::postResample(pred = MARSPred, obs = testing_observations)
+postResampTbl <- postResampTbl %>%
+  bind_rows(caret::postResample(pred = MARSPred, obs = testing_observations)  %>%
+              enframe %>%
+              spread(key=name,value=value) %>%
+              mutate(what = "MARS"))
 
-tibble(obs = testing_observations,
+mars_plot <- tibble(obs = testing_observations,
        pred = MARSPred) %>%
   ggplot(aes(x=obs,y=pred))+geom_point()+geom_abline()
 
-MARSModel %>% summary
+ggsave(mars_plot,
+       filename = file.path(output_directory,
+                            "mars-pred-plot.png"),
+       width = 8,
+       height = 6,
+       dpi = 100)
 
-plotmo(MARSModel$finalModel)
+# what does MARS do for V3? 
+
+mars_plot_func <- function(i)
+{
+  newdata <-predictors %>%
+    mutate_at(vars(-i),mean)
+  
+  newpred <- predict(MARSModel,
+                     newdata = newdata) %>%
+    as.tibble
+  
+  the_plot <- newdata %>% 
+    select(i) %>% 
+    bind_cols(newpred) %>% 
+    `names<-`(.,c("x","y")) %>% 
+    arrange(x) %>%
+    ggplot(aes(x=x,y=y)) + geom_line() + ggtitle(paste0("predictor ",i))
+  
+  ggsave(the_plot,
+         filename = file.path(output_directory,
+                              paste0("mars-var-",i,".png")),
+         width = 8,
+         height = 6,
+         dpi = 100)
+  
+  return(the_plot)
+}
+
+1:10 %>% purrr::map(mars_plot_func)
 
 # SVM
+
+SVMModel <- caret::train(x = predictors,
+                         y = raw_data$y,
+                         method = "svmRadial",
+                         preProc = c("center","scale"),
+                         tuneLength = 14,
+                         trControl = caret::trainControl(method = "cv"),
+                         tuneGrid = expand.grid(sigma = seq(0.01,0.10,by=0.01),
+                                                C = 2 ** (-2:11)))
+
+SVMPred <- predict(SVMModel, newdata = testing_predictors)
+
+postResampTbl <- postResampTbl %>%
+  bind_rows(
+    caret::postResample(pred = SVMPred, obs = testing_observations) %>%
+      enframe %>%
+      spread(key=name,value=value) %>%
+      mutate(what = "SVM"))
+
+svm_plot <- tibble(obs = testing_observations,
+       pred = SVMPred) %>%
+  ggplot(aes(x=obs,y=pred))+geom_point()+geom_abline()
+
+ggsave(svm_plot,
+       filename = file.path(output_directory,
+                            "svn-pred-plot.png"),
+       width = 8,
+       height = 6,
+       dpi = 100)
+
 # NN
+
+NNetModel <- caret::train(x = predictors,
+                          y = raw_data$y,
+                          method = "avNNet",
+                          tuneGrid = expand.grid(.decay = c(0.001,0.01,0.1),
+                                                 .size = 1:10,
+                                                 .bag = F),
+                          preProc = c("center","scale"),
+                          trControl = caret::trainControl(method = "cv"),
+                          linout = T,
+                          trace = F,
+                          MaxNWts = 10 * (ncol(predictors) + 1) + 10 + 1,
+                          maxit = 50,
+                          repeats = 200)
+
+NNPred <- predict(NNetModel, newdata = testing_predictors)
+
+postResampTbl <- postResampTbl %>%
+  bind_rows(
+    caret::postResample(pred = NNPred, obs = testing_observations) %>%
+      enframe %>%
+      spread(key=name,value=value) %>%
+      mutate(what = "avNNet"))
+
+
+av_nnet_plot <- tibble(obs = testing_observations,
+       pred = NNPred) %>%
+  ggplot(aes(x=obs,y=pred))+geom_point()+geom_abline()
+
+ggsave(av_nnet_plot,
+       filename = file.path(output_directory,
+                            "av-nnet-pred-plot.png"),
+       width = 8,
+       height = 6,
+       dpi = 100)
+
+knitr::kable(postResampTbl)
