033a527383c28663cf55381be32f2b0dd3361d8b
USER NAME <mark.t.douglas@gmail.com>
2019-12-26 12:27:13 +0000

    Looking at the oil models and finishing off 12.2

--- chapter-12/12.2/oils.r
+++ chapter-12/12.2/oils.r
@@ -4,11 +4,11 @@ data(oil)
 
 output_directory <- file.path(getwd(),"12.2")
 
-set.seed(1827777)
+set.seed(1827567)  # cherry picked so G is in both sets at least once
 
 TrainingIndices <- caret::createDataPartition(oilType,
                                               times = 1,
-                                              p = 0.7)$Resample1
+                                              p = 0.5)$Resample1
 
 TrainingPreds <- fattyAcids[TrainingIndices,]
 TrainingObs <- oilType[TrainingIndices]
@@ -16,7 +16,7 @@ TestingPreds <- fattyAcids[-TrainingIndices,]
 TestingObs <- oilType[-TrainingIndices]
 
 ctrl <- caret::trainControl(method = "cv",
-                            classProbs = T,
+                            classProbs = TRUE,
                             summaryFunction = caret::multiClassSummary,
                             sampling = "up")
 
@@ -35,6 +35,14 @@ LDA <- caret::train(TrainingPreds,
                     trControl = ctrl,
                     preProc = c("center","scale"))
 
+PLSDA <- caret::train(TrainingPreds,
+                      TrainingObs,
+                      method = "pls",
+                      tuneGrid = expand.grid(.ncomp = 1:8),
+                      preProc = c("center","scale"),
+                      metric = "Accuracy",
+                      trControl = ctrl)
+
 GLM <- caret::train(TrainingPreds,
                     TrainingObs,
                     method = "glmnet",
@@ -43,3 +51,46 @@ GLM <- caret::train(TrainingPreds,
                     preProc = c("center","scale"),
                     trControl = ctrl,
                     metric = "Accuracy")
+
+NSC <- caret::train(x = TrainingPreds,
+                    y = TrainingObs,
+                    method = 'pam',
+                    tuneGrid = expand.grid(.threshold = 1:25),
+                    preProc = c('center','scale'),
+                    metric = 'Accuracy')
+
+# Now we have the models, form the predictions on the test set 
+
+models <- list(
+  NSC = NSC,
+  GLM = GLM,
+  LDA = LDA,
+  PLSDA = PLSDA,
+  LinReg = LinReg)
+
+summary_data <- models %>%
+  purrr::map(~ predict(.x, TestingPreds)) %>%
+  as_tibble %>%
+  mutate(Obs = TestingObs) %>%
+  gather(key = Model, value = Pred, -Obs) %>%
+  mutate_if(is.factor,as.character) %>%
+  mutate(match = Obs==Pred) %>%
+  mutate(Obs = as.factor(Obs), Pred = as.factor(Pred))
+
+oil_fit_plot <- summary_data %>%
+  ggplot(aes(x = Obs, fill = match)) + geom_bar() + facet_wrap(~Model)
+
+ggsave(plot = oil_fit_plot,
+       filename = file.path(output_directory,"oil_fit_plot.png"),
+       width = 12,
+       height = 8,
+       dpi = 100)
+
+# and what is the scoring like? 
+
+summary_data %>%
+  mutate(match = ifelse(match,1.0,0.0)) %>%
+  group_by(Model,Obs) %>%
+  summarise(group_score = sum(match)/n()) %>%
+  summarise(Score = mean(group_score)) %>%
+  knitr::kable(.)
--- chapter-12/README.md
+++ chapter-12/README.md
@@ -21,3 +21,21 @@ So the best overall is the logistic regression (really multinomial) model applie
 So the first question is whether we need to split data into training and test. The answer is yes - how else do you propose to validate the model? Some cunning is needed to deal with such imbalance. I am going to go for a resampling technique. So I will cut the data into training and test, and then upsample the training data so each class has the same number of observations. 
 
 Using accuracy on this fake data to tune the models should be OK. To evaluate the test set a different statistic will probably be better; let's go with "average accuracy" i.e. for each class in the test data calculate the accuracy of that class and then average the accuracies. So if performance on the rare classes is bad the measure will be poor. 
+
+Plugging in the models, tuning them, and plotting the hit/miss matching of predicted values against truth gives us:
+
+![class](12.2/oil_fit_plot.png)
+
+so most of the models are actually OK. PLSDA fails terribly with group G (not surprising - PLS doesn't like single representatives). But nothing to choose between the penalised GLM and the NSC. 
+
+Values of my custom metric are:
+
+|Model  |     Score|
+|:------|---------:|
+|GLM    | 0.9841270|
+|LDA    | 0.9365079|
+|LinReg | 0.9206349|
+|NSC    | 0.9841270|
+|PLSDA  | 0.7936508|
+
+so have to use something else as the tie breaker. Why not `ties.method = 'first'`? GLM it is!
